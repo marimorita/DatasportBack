cd93cb395817def45130a2ca6aeb6175b6cd4a5f
SergioChica <sergioanjaci@gmail.com>
2024-08-27 14:47:35 -0500

    connect bd

--- src/data/mysql/ormconfig.ts
+++ src/data/mysql/ormconfig.ts
@@ -2,16 +2,16 @@ import { DataSource } from "typeorm";
 import { ClientsEntity } from "../entities/auth/clients/clients.entity";
 import { EmployeesEntity } from "../entities/auth/employees/employees.entity";
 import { AdministratorEntity } from "../entities/auth/administrator/administrator.entity";
-
+import { envs } from "../../config";
 
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
+    host: envs.DB_HOST,
+    port: envs.DB_PORT,
+    username: envs.DB_USERNAME,
     password: "",
-    database: process.env.DB_DATABASE,
+    database: envs.DB_DATABASE,
     synchronize: false, // Desactiva en producción
     logging: false,
     entities: [ClientsEntity,EmployeesEntity,AdministratorEntity], // Agrega todas tus entidades aquí
@@ -28,7 +28,7 @@ AppDataSource.initialize()
     .then(() => {
         console.log("Database MySql connected");
     })
-    .catch((error) => {
+    .catch((error: any) => {
         console.log("Database MySql connection error");
         throw error;
     });
